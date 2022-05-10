914cdba9ab2730aa003d29cfa77b697f47199558
Nicolas Bercovsky <bercovsky.nm@gmail.com>
2022-05-09 23:06:28 -0300

    conexion con microservicios

--- src/main/java/com/ccoins/Bff/feign/CoinsFeign.java
+++ src/main/java/com/ccoins/Bff/feign/CoinsFeign.java
@@ -1,14 +1,10 @@
 package com.ccoins.Bff.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "${feign.coins-ms.name}", url = "${feign.coins-ms.url}")
 @RequestMapping("${feign.coins-ms.req-ma}")
 public interface CoinsFeign {
 
-    @GetMapping("demo")
-    ResponseEntity<String> demo();
 }
