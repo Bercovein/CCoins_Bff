3a1a5e73b534ef05b6c1e1bc19b24969008331c8
Nicolas Bercovsky <bercovsky.nm@gmail.com>
2022-05-29 21:00:08 -0300

    swagger config

--- src/main/java/com/ccoins/Bff/configuration/SwaggerConfig.java
+++ src/main/java/com/ccoins/Bff/configuration/SwaggerConfig.java
@@ -1,11 +1,11 @@
 package com.ccoins.Bff.configuration;
 
+import com.ccoins.Bff.controller.OauthController;
 import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -14,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.List;
 
 import static com.ccoins.Bff.configuration.security.JwtUtils.*;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @EnableSwagger2
@@ -29,11 +30,12 @@ public class SwaggerConfig {
                 .securitySchemes(Lists.newArrayList(apikey()))    // agregar autenticacion con jwt
                 .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.any())     // package base donde va a encontrar los controladores
+                .apis(basePackage(OauthController.class.getPackage().getName()))     // package base donde va a encontrar los controladores
                 .paths(PathSelectors.any())        // para cualquier path menos el de HomeController
                 .build();
 
     }
+
     private ApiKey apikey() {
 
         return new ApiKey(TOKEN, AUTHORIZATION, HEADER);
