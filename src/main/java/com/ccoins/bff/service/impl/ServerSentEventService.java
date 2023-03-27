431d861e4379ae9687648be3b976c6c822859ccf
Nicolas Bercovsky <bercovsky.nm@gmail.com>
2023-03-26 22:26:39 -0300

    SSE loguea error

--- src/main/java/com/ccoins/bff/service/impl/ServerSentEventService.java
+++ src/main/java/com/ccoins/bff/service/impl/ServerSentEventService.java
@@ -3,14 +3,12 @@ package com.ccoins.bff.service.impl;
 import com.ccoins.bff.dto.IdDTO;
 import com.ccoins.bff.feign.BarsFeign;
 import com.ccoins.bff.service.IServerSentEventService;
-import com.ccoins.bff.utils.HeaderUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
@@ -20,6 +18,7 @@ import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
+@Slf4j
 public class ServerSentEventService implements IServerSentEventService {
 
     private BarsFeign barsFeign;
@@ -72,7 +71,7 @@ public class ServerSentEventService implements IServerSentEventService {
                     emitter.send(SseEmitter.event().name(eventName).data(data, MediaType.APPLICATION_JSON));
                 }catch(IOException e){
                     sseEmitterMap.remove(client);
-                    e.printStackTrace();
+                    log.error("Error while sending event to client.");
                 }
             });
             emitters.put(barId, sseEmitterMap);
@@ -92,7 +91,7 @@ public class ServerSentEventService implements IServerSentEventService {
                     sseEmitterMap.get(client).send(SseEmitter.event().name(eventName).data(data, MediaType.APPLICATION_JSON));
                 }catch(IOException e){
                     sseEmitterMap.remove(client);
-                    e.printStackTrace();
+                    log.error("Error while sending event to client.");
                 }
             });
             emitters.put(barId, sseEmitterMap);
