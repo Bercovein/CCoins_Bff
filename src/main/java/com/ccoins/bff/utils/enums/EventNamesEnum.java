2b34978c51969f52bfdccdd3754d9db38912fa45
Nicolas Bercovsky <bercovsky.nm@gmail.com>
2023-06-12 23:32:19 -0300

    login event deleted

--- src/main/java/com/ccoins/bff/utils/enums/EventNamesEnum.java
+++ src/main/java/com/ccoins/bff/utils/enums/EventNamesEnum.java
@@ -14,8 +14,6 @@ public enum EventNamesEnum {
     YOU_ARE_THE_LEADER("¡Felicidades! Eres el líder de la mesa."), //cambió el lider de la mesa
     NEW_CLIENT_TO_PARTY("El integrante %s se ha sumado a la mesa."), //se suma un cliente a la mesa
     CLIENT_LEFT_THE_PARTY("El integrante %s se ha retirado de la party."), //se suma un cliente a la mesa
-    LOGIN_CLIENT("Bienvenido a la party %s!"), //se suma un cliente a la mesa
-
     LOGOUT_CLIENT(null); // se elimina el cliente de la mesa
 
     private final String message;
