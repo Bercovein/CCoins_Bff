0bea7e872b490511b85962bf02415d502dfa1170
Bercovein <bercovsky.nm@gmail.com>
2023-06-29 00:25:46 -0300

    docker compose anda!

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo rm -r data
+# sudo rm -r data
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file ./.env -f docker-compose.yml up --build --remove-orphans
 # COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose up  -d
